69416b9b0a6bb0caa619cb86ed315f72a2ab56bb
Vit_QA <qa.nes.work@gmail.com>
2026-07-30 13:46:25 +0300

    Add howework_1

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let number = 2;
+let string = 'Hello, World!';
+let boolean = true;
+let nullValue = null;
+let undefinedValue = undefined;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nullValue);
+console.log(undefinedValue);
