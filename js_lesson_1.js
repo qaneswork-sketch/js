39e99b557afd4204e6ea4e2b04cc2df63159b037
Vit_QA <qa.nes.work@gmail.com>
2026-09-06 21:25:02 +0300

    Add howework_9_API_test

--- js_lesson_1.js
+++ js_lesson_1.js
@@ -1,1 +1,19 @@
-//Some code
+function* fibonacciGenerator() {
+	let prev = 0, next = 1;
+    while (true) {
+        yield prev;
+        let temp = prev;
+        prev = next;
+        next = temp + next; //[prev, next] = [next, prev + next] - деструктуризация
+    }
+}
+
+const fibGen = fibonacciGenerator();
+
+console.log(fibGen.next().value); // 0
+console.log(fibGen.next().value); // 1
+console.log(fibGen.next().value); // 1
+console.log(fibGen.next().value); // 2
+console.log(fibGen.next().value); // 3
+console.log(fibGen.next().value); // 5
+console.log(fibGen.next().value); // 8
